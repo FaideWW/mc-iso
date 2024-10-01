387cbc00c04e2c58851825b11aaa25bfa9469520
Michael Ko <gigaspyder@gmail.com>
2024-10-01 16:47:42 -0600

    first attempt at parser code (to be redone)

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -13,7 +14,18 @@ func main() {
 		log.Fatal("missing path to world dir")
 	}
 
-	inDir := args[1]
+	worldPath := args[1]
 
-	fmt.Printf("inDir: %s\n", inDir)
+	fmt.Printf("worldPath: %s\n", worldPath)
+
+	levelDatPath := filepath.Join(worldPath, "level.dat")
+
+	f, err := os.Open(levelDatPath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	ParseNBT(f)
+
 }
